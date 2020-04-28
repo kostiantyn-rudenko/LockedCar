24102250032fcc766bf7888df01595746662c5f5
Руденко Константин <kontsantin.rudenko@vokigames.com>
2020-04-28 23:51:07 +0300

    добавил скрипт для колес. добавил его к колесам - колеса крутятся!)

---
+++ Assets/Scripts/Wheel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wheel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float _rotationIndex = 30f;
+    private Mover _carMover;
+    private void Start()
+    {
+        _carMover = GetComponentInParent<Mover>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (_carMover == null)
+        {
+            Debug.Log("ERROR: no parent (car) for wheel");
+            return;
+        }
+
+        if (_carMover.curSpeed != 0f)
+            transform.Rotate(new Vector3(0f, 0f, _rotationIndex * _carMover.curSpeed));
+    }
+}
