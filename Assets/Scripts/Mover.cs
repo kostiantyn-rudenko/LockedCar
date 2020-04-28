4fea1ef470b83c604c85284afb45627c9ee8b24a
Руденко Константин <kontsantin.rudenko@vokigames.com>
2020-04-28 23:50:20 +0300

    рефакторинг кода + фикс бага с проезжанием сквозь стены

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -5,36 +5,33 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField]
-    private float maxSpeed = 0.5f; // абсолютное значение максимальной скорости (при движкнии назад -maxSpeed)
-    [SerializeField]
-    private float acceleration = 0.05f; // ускорение
-    [SerializeField]
-    private float friction = 0.01f; // трение. на каждом апдейте это значение отнимается от скорости (или прибавляется, если скорость отрицательна)
+    [SerializeField] private float _maxSpeed = 0.3f; // абсолютное значение максимальной скорости (при движкнии назад -_maxSpeed)
+    [SerializeField] private float _acceleration = 0.05f; // ускорение
+    [SerializeField] private float _friction = 0.01f; // трение. на каждом апдейте это значение отнимается от скорости (или прибавляется, если скорость отрицательна)
     // все то же, только для угла поворота
-    [SerializeField]
-    private float stepRotation = 0.2f;
-    [SerializeField]
-    private float frictionRotation = 0.1f;
-    [SerializeField]
-    private float maxRotation = 3f;
+    [SerializeField] private float _stepRotation = 0.2f;
+    [SerializeField] private float _frictionRotation = 0.1f;
+    [SerializeField] private float _maxRotation = 3f;
 
 
-
-    private float curSpeed = 0f; // текущая скорость
-    private float curRotation = 0f; // текущий угол поворота
+    public float curSpeed { get; private set; } // текущая скорость
+    public float curRotation { get; private set; }  // текущий угол поворота
     
-    private bool collisionExists = false; // присутствует ли столкновение в данный момент
-
+    private int collisionsCount = 0; // колличество коллизий в данный момент времени
     
-    public void FixedUpdate()
+    // если я правильно понимаю, этот метод нужно использовать вместо конструктора для классов-наследников MonoBehaviour
+    private void Awake() {
+        curSpeed = 0f;
+        curRotation = 0f;
+    }
+
+    private void FixedUpdate()
     {   
         // пока присутствует коллизия - игнорируем нажатия клавиш
-        if (!collisionExists) {
+        if (collisionsCount <= 0) {
             UpdateSpeed();
             UpdateAngle();
         }
-
         // если машинка движется назад, стрелка влево поворачивает направо и наоборот (как колеса в машине)
         // + если скорость = 0 - машинка не может поворачивать
         float ang = curRotation;
@@ -46,17 +43,21 @@ public class Mover : MonoBehaviour
         transform.Translate(new Vector3(-curSpeed, 0f, 0f));
     }
 
-    void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision)
     {
-        curSpeed = -curSpeed;
-        curRotation = -curRotation;
-
-        collisionExists = true;
+        // этот блок меняет направление движения в момент столкновения, чтоб машинка не заехала за препятствие
+        // меняем только при первом столкновении т.к. может быть 2 одновременно и тогда направление скорости не 
+        // поменяется и машинка "проскользнет"
+        if (collisionsCount == 0) {
+            curSpeed = -curSpeed;
+            curRotation = -curRotation;
+        }
+        collisionsCount++;
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        collisionExists = false;
+        collisionsCount--;
     }
 
     private void UpdateSpeed()
@@ -64,7 +65,7 @@ public class Mover : MonoBehaviour
         bool need_add_friction = true;
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            curSpeed = MathClamp(curSpeed + acceleration - friction, 0, maxSpeed);
+            curSpeed = Mathf.Clamp(curSpeed + _acceleration - _friction, 0, _maxSpeed);
             need_add_friction = false;
 
             if (Input.GetKey(KeyCode.DownArrow))
@@ -74,25 +75,25 @@ public class Mover : MonoBehaviour
         }
         else if (Input.GetKey(KeyCode.DownArrow))
         {
-            curSpeed = MathClamp(curSpeed - acceleration + friction, -maxSpeed, 0);
+            curSpeed = Mathf.Clamp(curSpeed - _acceleration + _friction, -_maxSpeed, 0);
             need_add_friction = false;
         }
 
         if (Input.GetKey(KeyCode.Space))
         {
             if (curSpeed > 0f)
-                curSpeed = MathClamp(curSpeed - acceleration - friction, 0f, maxSpeed);
+                curSpeed = Mathf.Clamp(curSpeed - _acceleration - _friction, 0f, _maxSpeed);
             else if (curSpeed < 0f)
-                curSpeed = MathClamp(curSpeed + acceleration + friction, -maxSpeed, 0f);
+                curSpeed = Mathf.Clamp(curSpeed + _acceleration + _friction, -_maxSpeed, 0f);
             need_add_friction = false;
         }
 
         if (need_add_friction && curSpeed != 0f)
         {
             if (curSpeed > 0f)
-                curSpeed = MathClamp(curSpeed - friction, 0, maxSpeed);
+                curSpeed = Mathf.Clamp(curSpeed - _friction, 0, _maxSpeed);
             else if (curSpeed < 0f)
-                curSpeed = MathClamp(curSpeed + friction, -maxSpeed, 0);
+                curSpeed = Mathf.Clamp(curSpeed + _friction, -_maxSpeed, 0);
         }
     }
 
@@ -100,28 +101,18 @@ public class Mover : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            curRotation = MathClamp(curRotation + stepRotation, 0, maxRotation);
+            curRotation = Mathf.Clamp(curRotation + _stepRotation, 0, _maxRotation);
         }
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
-            curRotation = MathClamp(curRotation - stepRotation, -maxRotation, 0);
+            curRotation = Mathf.Clamp(curRotation - _stepRotation, -_maxRotation, 0);
         }
         else
         {
             if (curRotation > 0f)
-                curRotation = MathClamp(curRotation - frictionRotation, 0, maxRotation);
+                curRotation = Mathf.Clamp(curRotation - _frictionRotation, 0, _maxRotation);
             else if (curRotation < 0f)
-                curRotation = MathClamp(curRotation + frictionRotation, -maxRotation, 0);
+                curRotation = Mathf.Clamp(curRotation + _frictionRotation, -_maxRotation, 0);
         }
-    }
-
-    private float MathClamp(float value, float min, float max)
-    {
-        if (value < min)
-            return min;
-        else if (value > max)
-            return max;
-        else
-            return value;
     }
 }
