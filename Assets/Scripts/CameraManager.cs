48611968e2de2e3694f5c38b64f688008382975c
Руденко Константин <kontsantin.rudenko@vokigames.com>
2020-04-29 15:52:38 +0300

    добавил камеру на машинку. добавил переключение камер кнопками "1" и "2"

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    [SerializeField] private Camera[] _cameras;
+    private int _currentCameraNumber = 0;
+
+    void LateUpdate() 
+    {   
+        for (int i = 0; i < _cameras.Length; i++) {
+            if (Input.GetKey(KeyCode.P)) {
+                Debug.Log("checking " + i.ToString());
+
+            }
+            if (Input.GetKeyUp((i + 1).ToString())) {
+                Debug.Log("Switch camera to " + i.ToString());
+                SwitchCamera(i);
+                return;
+            }
+        }
+    }
+
+    private void SwitchCamera(int number) {
+        if (_currentCameraNumber == number)
+            return;
+
+        _cameras[_currentCameraNumber].enabled = false;
+        _cameras[number].enabled = true;
+        _currentCameraNumber = number;
+    }
+}
